d20f52d889a5131b9c3a3d688243fdef4eec5b4d
phoenix-da-phenom <ehinormichael@ymail.com>
2025-01-04 17:24:08 +0100

    Watch Live

--- src/Components/WatchLive.jsx
+++ src/Components/WatchLive.jsx
@@ -2,15 +2,16 @@ import React from "react";
 
 export default function WatchLive() {
   return (
-    <div>
-      <div>
-        <div>
-            <h1>Watch Live</h1>
-
+    <div className="container">
+      <h2 className="container">Stay in touch online now!</h2>
+      <div className="container">
+        <div id="buttons">
+           <button>Watch Videos</button>
+          <button>Watch Live {"(Streaming)"}</button>
         </div>
-        <div>
+        <div id="image">
 
-          <p>Picture</p>
+          <img src='./Image/archive/PHOTO-2022-11-20-12-38-28.jpg' alt='image' />
         </div>
       </div>
     </div>
