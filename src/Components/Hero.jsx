19c030672d4576d9b0a839fa508a1e0208ae5ce6
phoenix-da-phenom <ehinormichael@ymail.com>
2025-01-04 16:11:09 +0100

    another  commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ import NavigationalBar from "./NavigationalBar";
 export default function Hero() {
   return (
     <div className="mt-5">
-     <div className="h-[200px] w-full bg-[url('./Image/archive/PHOTO-2022-11-20-12-38-28.jpg')] opacity-70 -z-10 p-8">
+     <div className="h-[200px] w-full bg-[url('./Image/archive/PHOTO-2022-11-20-12-38-28.jpg')] -z-10 p-8">
     </div>
     <NavigationalBar />
     <HeroText />
--- src/Components/WatchLive.jsx
+++ src/Components/WatchLive.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function WatchLive() {
   return (
-    <div>WatchLive</div>
-  )
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
