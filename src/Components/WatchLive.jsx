62713797e658690c71dba25e2fdd67904e3e8f48
phoenix-da-phenom <ehinormichael@ymail.com>
2025-01-04 16:57:54 +0100

    Adding watch live component to home page

--- src/Components/WatchLive.jsx
+++ src/Components/WatchLive.jsx
@@ -4,8 +4,14 @@ export default function WatchLive() {
   return (
     <div>
       <div>
-        <div></div>
-        <div></div>
+        <div>
+            <h1>Watch Live</h1>
+
+        </div>
+        <div>
+
+          <p>Picture</p>
+        </div>
       </div>
     </div>
   );
