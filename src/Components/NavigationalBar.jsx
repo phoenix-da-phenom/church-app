5345ddec5fd4ad8e651aec9c58f07175e73ffe0e
phoenix-da-phenom <ehinormichael@ymail.com>
2025-01-05 11:00:25 +0100

    Updating the navigation bar( not working well at the moment)

--- src/Components/NavigationalBar.jsx
+++ src/Components/NavigationalBar.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import Logo from '../Image/logo.png'
+import BurgerIcon from '../Image/menu.png'
+import CloseIcon from '../Image/close.png'
 
 export default function NavigationalBar() {
+  const [currentClass, setCurrentClass] = React.useState("hidden");
+  const ShowMenu=()=>{
+    console.log("clicked")
+   setCurrentClass(" ")
+  }
+  const HideMenu=()=>{
+    setCurrentClass("hidden")
+
+  }
+
+
   return (
-    <div className="flex flex-row justify-between items-center container pt-3" >
+    <div className="flex flex-row justify-between items-center container pt-3 pr-16" >
       <div className="flex-row   space-x-20">
         <div className="w-12 h-6 ">
           <img src={Logo} alt="" />
@@ -14,21 +27,51 @@ export default function NavigationalBar() {
 
     
         <ul className="hidden lg:flex flex-row space-x-20">
-          <li class="group hover:-translate-y-2 transition-transform duration-300">
+          <li className="group hover:-translate-y-2 transition-transform duration-300">
             <a href="/" 
             className="text-lg font-semibold  hover:text-accent-main">Home</a>
           </li>
-          <li class="group hover:-translate-y-2 transition-transform duration-300">
+          <li className="group hover:-translate-y-2 transition-transform duration-300">
             <a href="/about"
              className="text-lg font-semibold  hover:text-accent-main"
             >About</a>
           </li>
-          <li class="group hover:-translate-y-2 transition-transform duration-300">
+          <li className="group hover:-translate-y-2 transition-transform duration-300">
             <a href=""
               className="text-lg font-semibold  hover:text-accent-main"
             >Gallery</a>
           </li>
         </ul>
+
+        <div id="mobileNav" className={`${currentClass} hidden w-[100%] h-[100%] bg-black text-white fixed top-0 left-0 z-50 place-items-center pt-32`}>
+
+        <div id="closeIcon">
+          <img src={CloseIcon} alt="" className="w-10 h-10 absolute right-10 top-10 cursor-pointer" />
+        </div>
+
+        <ul className="flex flex-col gap-4 space-y-4 text-center">
+          <li className="group hover:-translate-y-2 transition-transform duration-300">
+            <a href="/" 
+            className="text-lg font-semibold  hover:text-accent-main">Home</a>
+          </li>
+          <li className="group hover:-translate-y-2 transition-transform duration-300">
+            <a href="/about"
+             className="text-lg font-semibold  hover:text-accent-main"
+            >About</a>
+          </li>
+          <li className="group hover:-translate-y-2 transition-transform duration-300">
+            <a href=""
+              className="text-lg font-semibold  hover:text-accent-main"
+            >Gallery</a>
+          </li>
+        </ul>
+        </div>
+
+        <div id="burgerHnd" className="w-6 flex cursor-pointer lg:hidden" onClick={ShowMenu}>
+              <img src={BurgerIcon}/>
+              Menu
+
+        </div>
     
     </div>
   );
