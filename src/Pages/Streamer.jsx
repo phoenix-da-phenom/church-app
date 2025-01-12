439041ab45d79bbb4e6e02be723e6d17b841c39c
phoenix-da-phenom <ehinormichael@ymail.com>
2025-01-12 11:40:13 +0100

    fix io socket url

--- src/Pages/Streamer.jsx
+++ src/Pages/Streamer.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // Connect to the signaling server
+const socket = io("https://church-backend-58s3.onrender.com"); // Connect to the signaling server
 
 const Streamer = () => {
   const videoRef = useRef(null);
