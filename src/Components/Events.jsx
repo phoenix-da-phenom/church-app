54add5bda5420580b8e559a2d9008ee1c7e076b8
phoenix-da-phenom <ehinormichael@ymail.com>
2025-01-17 20:14:27 +0100

    Carousel event implemented

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-import p1 from '../Image/next_up1.jpg'
-import p2 from '../Image/PHOTO-2022-11-20-12-37-22.jpg'
-import p3 from '../Image/archive/PHOTO-2022-11-20-12-38-28.jpg'
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+
+
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+
+import p1 from '../Image/next_up1.jpg';
+import p2 from '../Image/PHOTO-2022-11-20-12-37-22.jpg';
+import p3 from '../Image/archive/PHOTO-2022-11-20-12-38-28.jpg';
 
 export default function Events() {
   return (
@@ -10,167 +22,111 @@ export default function Events() {
         Upcoming Events
       </h1>
 
-      <div className=" p-5">
-        {/* Header Section */}
-        <div className="space-y-5 ">
-          <div className="bg-primary-main rounded-2xl p-2">
-            <h1 className="text-center font-extrabold shadow-inner">
-              1st 8th 15th 22nd 29 January 2025
-            </h1>
+      <Swiper
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <div className="p-5">
+            <div className="space-y-5">
+              <div className="bg-primary-main rounded-2xl p-2">
+                <h1 className="text-center font-extrabold shadow-inner">
+                  1st 8th 15th 22nd 29 January 2025
+                </h1>
+              </div>
+              <h4 className="text-center font-extrabold text-gray-500">
+                <span className="text-red-600">JANUARY</span> Must Obey
+              </h4>
+            </div>
+
+            <div id="description" className="p-4 rounded-md shadow-md mt-4 mb-8">
+              <img
+                src={p1}
+                alt="Christmas Eve Service"
+                className="float-left w-full md:w-1/2 lg:w-1/6 h-auto mr-4 mb-4 rounded-md object-cover"
+              />
+              <p className="text-gray-400 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+                deleniti architecto consectetur maiores dolore molestias...
+              </p>
+            </div>
           </div>
-          <h4 className="text-center font-extrabold text-gray-500">
-            <span className="text-red-600">JANUARY</span> Must Obey
-          </h4>
-        </div>
-
-        {/* Description Section */}
-        <div
-          id="description"
-          className="p-4  rounded-md shadow-md mt-4 mb-8"
-        >
-          {/* Image with Float */}
-          <img
-            src={p1}
-            alt="Christmas Eve Service"
-            className="float-left w-full md:w-1/2 lg:w-1/6 h-auto mr-4 mb-4 rounded-md object-cover"
-          />
-
-          {/* Text */}
-          <p className="text-gray-400 text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            deleniti architecto consectetur maiores dolore molestias, corporis,
-            consequatur veniam blanditiis impedit iste aut omnis nam itaque enim
-            accusantium voluptatum iusto ipsum! Reprehenderit suscipit quia
-            dolorum, quis itaque, quas unde, laborum porro doloremque inventore
-            amet architecto. Deleniti neque voluptatibus consectetur numquam
-            totam facilis aspernatur eum asperiores vero dicta libero tempora,
-            aliquam ea. Fuga aut dolores vero facilis, quas eligendi
-            reprehenderit ratione porro minus magni itaque laborum incidunt, in
-            quos maiores perspiciatis, voluptatem dolorum. Cupiditate
-            dignissimos animi, numquam non dolorum id consequatur facere! Omnis
-            fugiat porro quibusdam voluptatem aut officia a veritatis placeat
-            nulla ex doloribus harum cum sapiente illo ipsum qui rem, itaque sit
-            ipsam sed eligendi vel laudantium temporibus praesentium. Ipsum.
-            Porro tempore mollitia nulla, facilis praesentium ratione amet eaque
-            non similique officiis quam voluptatem dicta. Maxime pariatur veniam
-            quo, quis, dolor voluptas tenetur molestiae optio, in quod nulla nam
-            sunt?
-          </p>
-        </div>
-      </div>
-
-
-
-
-
-      <div className="p-5">
-        {/* Header Section */}
-        <div className="space-y-5">
-  <div className="bg-primary-main rounded-2xl p-2">
-    <h1 className="text-center font-extrabold shadow-inner">3 April 2025</h1>
-  </div>
-  <h4 className="text-center font-extrabold text-gray-500">
-    <span className="text-red-600">Easter</span> Service
-  </h4>
-</div>
-
-{/* Description Section */}
-<div
-  id="description"
-  className="p-4 rounded-md shadow-md mt-4 mb-8"
->
-  {/* Image with Float */}
-  <img
-    src={p2}
-    alt="Easter"
-    className="float-left w-48 sm:w-64 md:w-72 lg:w-44 h-auto mr-4 mb-4 rounded-md object-cover"
-  />
-
-  {/* Text */}
-  <p className="text-gray-400 text-justify">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-    deleniti architecto consectetur maiores dolore molestias, corporis,
-    consequatur veniam blanditiis impedit iste aut omnis nam itaque enim
-    accusantium voluptatum iusto ipsum! Reprehenderit suscipit quia
-    dolorum, quis itaque, quas unde, laborum porro doloremque inventore
-    amet architecto. Deleniti neque voluptatibus consectetur numquam
-    totam facilis aspernatur eum asperiores vero dicta libero tempora,
-    aliquam ea. Fuga aut dolores vero facilis, quas eligendi
-    reprehenderit ratione porro minus magni itaque laborum incidunt, in
-    quos maiores perspiciatis, voluptatem dolorum. Cupiditate
-    dignissimos animi, numquam non dolorum id consequatur facere! Omnis
-    fugiat porro quibusdam voluptatem aut officia a veritatis placeat
-    nulla ex doloribus harum cum sapiente illo ipsum qui rem, itaque sit
-    ipsam sed eligendi vel laudantium temporibus praesentium. Ipsum.
-    Porro tempore mollitia nulla, facilis praesentium ratione amet eaque
-    non similique officiis quam voluptatem dicta. Maxime pariatur veniam
-    quo, quis, dolor voluptas tenetur molestiae optio, in quod nulla nam
-    sunt?
-  </p>
-</div>
-
-
-
-
-
-
-
-        <div>
-        {/* Header Section */}
-        <div className="space-y-5">
-          <div className="bg-primary-main rounded-2xl p-2">
-            <h1 className="text-center font-extrabold shadow-inner">
-              24 June 2025
-            </h1>
+        </SwiperSlide>
+        
+        {/* Add additional slides similarly */}
+        <SwiperSlide>
+          <div className="p-5">
+            <div className="space-y-5">
+              <div className="bg-primary-main rounded-2xl p-2">
+                <h1 className="text-center font-extrabold shadow-inner">3 April 2025</h1>
+              </div>
+              <h4 className="text-center font-extrabold text-gray-500">
+                <span className="text-red-600">Easter</span> Service
+              </h4>
+            </div>
+            <div id="description" className="p-4 rounded-md shadow-md mt-4 mb-8">
+              <img
+                src={p2}
+                alt="Easter"
+                className="float-left w-48 sm:w-64 md:w-72 lg:w-44 h-auto mr-4 mb-4 rounded-md object-cover"
+              />
+              <p className="text-gray-400 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit...
+              </p>
+            </div>
           </div>
-          <h4 className="text-center font-extrabold text-gray-500">
-            <span className="text-red-600">Mid Year</span> Thanks Giving Service
-          </h4>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="p-5">
+            <div className="space-y-5">
+              <div className="bg-primary-main rounded-2xl p-2">
+                <h1 className="text-center font-extrabold shadow-inner">3 April 2025</h1>
+              </div>
+              <h4 className="text-center font-extrabold text-gray-500">
+                <span className="text-red-600">Easter</span> Service
+              </h4>
+            </div>
+            <div id="description" className="p-4 rounded-md shadow-md mt-4 mb-8">
+              <img
+                src={p2}
+                alt="Easter"
+                className="float-left w-48 sm:w-64 md:w-72 lg:w-44 h-auto mr-4 mb-4 rounded-md object-cover"
+              />
+              <p className="text-gray-400 text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit...
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
         </div>
-
-        {/* Description Section */}
-        <div
-          id="description"
-          className="p-4 border border-gray-200 rounded-md shadow-md mt-4 mb-8"
-        >
-          {/* Image with Float */}
-          <img
-            src={p3}
-            alt="Mid Year"
-            className="float-left w-full md:w-1/2 lg:w-1/3 h-auto mr-4 mb-4 rounded-md object-cover"
-          />
-
-          {/* Text */}
-          <p className="text-gray-400 text-justify ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            deleniti architecto consectetur maiores dolore molestias, corporis,
-            consequatur veniam blanditiis impedit iste aut omnis nam itaque enim
-            accusantium voluptatum iusto ipsum! Reprehenderit suscipit quia
-            dolorum, quis itaque, quas unde, laborum porro doloremque inventore
-            amet architecto. Deleniti neque voluptatibus consectetur numquam
-            totam facilis aspernatur eum asperiores vero dicta libero tempora,
-            aliquam ea. Fuga aut dolores vero facilis, quas eligendi
-            reprehenderit ratione porro minus magni itaque laborum incidunt, in
-            quos maiores perspiciatis, voluptatem dolorum. Cupiditate
-            dignissimos animi, numquam non dolorum id consequatur facere! Omnis
-            fugiat porro quibusdam voluptatem aut officia a veritatis placeat
-            nulla ex doloribus harum cum sapiente illo ipsum qui rem, itaque sit
-            ipsam sed eligendi vel laudantium temporibus praesentium. Ipsum.
-            Porro tempore mollitia nulla, facilis praesentium ratione amet eaque
-            non similique officiis quam voluptatem dicta. Maxime pariatur veniam
-            quo, quis, dolor voluptas tenetur molestiae optio, in quod nulla nam
-            sunt?
-          </p>
-        </div>
-      </div>
-      </div>
-
-
-
-
-
-
-
+      </Swiper>
     </div>
   );
 }
